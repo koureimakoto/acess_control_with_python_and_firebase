38938fbc6e6de0437827d007d5b33691f1625764
Igor Michetti <michettigor@gmail.com>
2022-10-30 22:07:14 -0400

    Funções pendente do módulo main_options criadas

--- src/main_options.py
+++ src/main_options.py
@@ -1,4 +1,6 @@
 from enum     import Enum
+from ssl import _PasswordType
+from turtle import update
 from typing   import Literal
 from auth     import User
 from platform import platform
@@ -11,17 +13,29 @@ class Terminal(Enum):
 
 
 def create_user(user: User) -> bool:
-    """
-    Operações de Criar um Novo usuário 
-    """
-    pass
+    
+    print("Criar novo usuario\n")
+    email = input("Digite o Email: ")
+    password = input("Digite a Senha: ")
+    password_conf = input("Confirme a Senha: ")
+    
+    if password == password_conf:
+        user.create_user_with_email_and_password(email, password)
+        print("Usuário criado.")
+        return True
 
+    else:
+        print("Senhas não correspondem, tente novamente.\n")
+        create_user(user)
 
 def update_user(user: User) -> bool:
-    """
-    Operações para Atualizar informações do usuário
-    """
-    pass
+    update_password = input("Deseja alterar a senha?(S/N)\n").upper()
+    if update_password == "S":
+        user.send_password_reset_email(user.get_email)
+        return True
+    else:
+        "Operação cancelada."
+        return False
 
 def delete_user(user: User) -> bool:
     """
@@ -30,15 +44,25 @@ def delete_user(user: User) -> bool:
     @@ ou talvez pedindo para ele redigitar o e-mail e
     @@ para forçar a decisão
     """
-    pass
+    user_delete = input("Deseja deletar o usuario?(S/N)\n").upper()
+    if user_delete == "S":
+        user.delete_user_account(user.get_id_token())
+        return True
+    else:
+        "Operação cancelada."
+        return False
+    
 
 def verify_user(user: User) -> bool:
     """
-    Operações que retorna se u usuário está ou não
+    Operações que retorna se o usuário está ou não
     cadastrado e caso não esteja retorna o usuário
     retorna menu inicial.
     """
-    pass
+    if user.get_email_verification:
+        return True
+    else:
+        return False
 
 def char_to_int( char: str ) -> int | bool:
     """
